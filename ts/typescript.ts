a401cd7ee37b28ac41623e3c4f42e32be2be1dd6
Slava Metelkova <slavametelkova16@gmail.com>
2023-06-17 23:40:20 +0200

    num

--- ts/typescript.ts
+++ ts/typescript.ts
@@ -2,3 +2,11 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 console.log(sum(2, 3));
+
+function isPalindrome(str: string): boolean;
+{
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+console.log(inPalindrome("radar"));
+console.log(isPalindrome("hello"));
